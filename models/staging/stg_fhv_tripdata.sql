b31079558e8370decace509166fe1fd72b81167b
Enoch Kim <Enkim2012@gmail.com>
2023-02-24 18:56:55 +0000

    files

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -15,6 +15,6 @@ from {{ source('staging', 'fhv_tripdata') }}
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
-    limit 100
+  limit 100
 
 {% endif %}
